62eed080b2a653d468e1e14d4c12b27d84aa57fe
Nziza-Prince <nzizaprince7@gmail.com>
2025-04-24 20:48:44 +0200

    started with react query

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios"
 import { useEffect, useState } from "react";
 
@@ -8,32 +9,23 @@ interface Todo{
 }
 
 const TodoList = () => {
+  const fetchTodos = async () => {
+    const res = await axios.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+    return res.data.slice(0,15)
+  }
+const {data:todos} = useQuery({
+  queryKey : ['todos'],
+  queryFn:fetchTodos
+})
 
-  const [todos,setTodos] = useState<Todo[]>([])
-  const [error,setError] = useState('')
-
-  useEffect(()=>{
-    const fetchTodos = async ()=>{
-      try{
-
-        const response = await axios.get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-        setTodos(response.data.slice(0,10))
-      }catch(e:any){
-        setError("Failed to fetch the todos")
-        console.error(e.message)
-      }
-    }
-    fetchTodos()
-  },[])
 
   return (
     <div className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-md mt-10">
       <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">📝 Todo List</h2>
-      
-      {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+
       
       <ul className="space-y-3">
-        {todos.map(todo => (
+        {todos?.map(todo => (
           <li
             key={todo.id}
             className={`p-4 rounded-lg shadow flex items-center justify-between ${
