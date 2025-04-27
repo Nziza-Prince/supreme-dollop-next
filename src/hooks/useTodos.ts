7de38fb774d70ad724255c040d61f42a2d67eb88
Nziza-Prince <nzizaprince7@gmail.com>
2025-04-27 11:02:53 +0200

    fixed the syntax and type errors

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -8,14 +8,7 @@ const apiClient = new APIClient('/todos')
 
 
 const useTodos = () => {
-    const fetchTodos = async () => {
-        const res = await axios.get<Todo[]>(
-          "https://jsonplaceholder.typicode.com/todos"
-        );
-        return res.data.slice(0, 15);
-      };
-
-      return useQuery({
+      return useQuery<Todo [], Error>({
         queryKey: CACHE_KEY_TODOS,
         queryFn: apiClient.getAll,
         staleTime : 10*1000
